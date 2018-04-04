67a58945aec117897fb467c9e3d5a983cec13825
Bianca91 <biancasteyn91@gmail.com>
2018-04-04 14:14:02 +0200

    Add authorization to student and class end points

--- src/class/controller.ts
+++ src/class/controller.ts
@@ -3,11 +3,14 @@ import {
   Post,
   HttpCode,
   Body,
+  Authorized
 } from "routing-controllers";
 import Classes from "./entity";
 
 @JsonController()
 export default class ClassController {
+
+  @Authorized()
   @Post("/class")
   @HttpCode(201)
   async createBatch(@Body() classes: Classes) {
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import 'reflect-metadata'
-import {createKoaServer} from "routing-controllers"
+import {createKoaServer } from "routing-controllers"
 import setupDb from './db'
 import ClassController from './class/controller'
 import StudentController from './students/controller'
 import LoginController from './login/controller'
+import {Action } from 'routing-controllers'
+import { verify } from './jwt'
+import UserController from './login/userController'
 
 const port = process.env.PORT || 4000
 
 const app = createKoaServer({
-   controllers: [ClassController, StudentController, LoginController]
+   controllers: [ClassController, StudentController, LoginController, UserController],
+
+   authorizationChecker: (action: Action) => {
+       const header: string = action.request.headers.authorization
+       if (header && header.startsWith('Bearer ')) {
+         const [ , token ] = header.split(' ')
+         return !!(token && verify(token))
+       }
+
+       return false
+     }
+
 })
 
 setupDb()
